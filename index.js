03921635b0bb35853b533d3ad4ef55ce242cd09d
Ajiboye Temitope <mrtayormi@outlook.com>
2019-11-11 12:16:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const express = require('express');
+const app = express();
+
+mongoose.connect('mongodb://localhost/vidly', { useUnifiedTopology: true, useNewUrlParser: true})
+.then(() => console.log('Connected to MongoDb...'))
+.catch(err => console.err('Error connecting to MongoDb'))
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ routes/customers.js
@@ -0,0 +1,27 @@
+const { Customer, validate} = require('../models/customer');
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/', async (req, res) => {
+    const customers = await Customer.find().sort('name');
+    res.send(customers);
+});
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let customer = new Customer({
+        name: req.body.name,
+        phone: req.body.phone,
+        isGold: req.body.isGold
+    });
+    customer = await Customer.save()
+    res.send(customer);
+});
+
+
+
+module.exports = router;
