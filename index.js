3b22c20085a815fac970f4b6e4f4c78fad03b5d1
Ajiboye Temitope <mrtayormi@outlook.com>
2019-11-12 14:52:49 +0100

    Create Users route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const users = require('./routes/users')
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
@@ -17,6 +18,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/users.js
+++ routes/users.js
@@ -4,47 +4,21 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', async (req, res) => {
-    const users = await User.find().sort('name');
-    res.send(users);
-});
-
 router.post('/', async (req, res) => {
     const { error } = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const user = new Genre({
-        name: req.body.name
+    let user = await User.findOne({ email: req.body.email});
+    if(user) return res.status(400).send('User already registered');
+
+    user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
     });
     await user.save()
     res.send(user);
 });
-
-// router.put('/:id', async (req, res) => {
-//     const { error } = validateGenre(req.body);
-//     if (error) return res.status(400).send(error.details[0].message);
-
-//     const genre = await User.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
-//         new: true
-//     })
-
-//     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-//     res.send(genre);
-// });
-
-// router.delete('/:id', async (req, res) => {
-//     const genre = Genre.findByIdAndRemove(req.params.id);
-//     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-//     res.send(genre);
-// });
-
-// router.get('/:id', async (req, res) => {
-//     const genre = await Genre.findById(req.params.id)
-//     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-//     res.send(genre);
-// });
 
 
 module.exports = router;
