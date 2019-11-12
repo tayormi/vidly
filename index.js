231f95cab33ccf8ed3288dad3c1488dcaecb2cb0
Ajiboye Temitope <mrtayormi@outlook.com>
2019-11-12 16:20:28 +0100

    Login route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const users = require('./routes/users')
+const auth = require('./routes/auth')
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
@@ -19,6 +20,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
