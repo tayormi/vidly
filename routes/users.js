f8ea87cead5ab27ae2bbddbb940702bcad35a762
Ajiboye Temitope <mrtayormi@outlook.com>
2019-11-12 16:11:54 +0100

    Hash passwords

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { User, validateUser } = require('../models/user');
 const mongoose = require('mongoose');
@@ -13,6 +14,9 @@ router.post('/', async (req, res) => {
     if(user) return res.status(400).send('User already registered');
 
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt)
     await user.save()
 
     res.send(_.pick(user, ['name', 'email']));
